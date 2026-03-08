cb523d41686ad406b7304168d9b08f9fe100afb1
bibabbly <bizimungu2004@gmail.com>
2026-03-08 11:17:09 +0200

    Update api url on the railway

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://stock-management-backend-production-6c53.up.railway.app/api',
 })
 
 // Automatically attach token to every request
